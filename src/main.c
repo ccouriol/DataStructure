18e5d3a512256c370065b9a3a7eb490c47a85f42
ccouriol <clement.couriol@cpe.fr>
2021-10-13 17:16:19 +0200

    feat: queue data structure

--- src/main.c
+++ src/main.c
@@ -18,10 +18,11 @@
 #include <assert.h>
 
 #include "../include/stack.h"
+#include "../include/queue.h"
 
 /*!
 * \fn float test(int a)
-* \author Name <email@email.com>
+* \author Clément couriol
 * \version 0.1
 * \date  13/10/2021
 * \brief 
@@ -54,4 +55,38 @@ int main(int argc, char **argv)
 {
 
     return (EXIT_SUCCESS);
+}
+
+void test_queue(int a)
+{
+    Queue qu;
+    init_queue(&qu);
+    enqueue(&qu, 1.4);
+    enqueue(&qu, 2.4);
+    enqueue(&qu, 3.4);
+    for (int i = 0; i < qu.index; i++)
+        printf("%f\n", qu.data[i]);
+    printf("%i\n", qu.index);
+
+    float dequ = dequeue(&qu);
+    puts("qu dequeue");
+
+    for (int i = 0; i < qu.index; i++)
+        printf("%f\n", qu.data[i]);
+
+    printf("dequ : %f\n", dequ);
+    printf("empty %i\n", is_queue_empty(&qu));
+    dequ = front(&qu);
+    printf("dequ : %f\n", dequ);
+    for (int i = 0; i < qu.index; i++)
+        printf("%f\n", qu.data[i]);
+
+    clear_queue(&qu);
+    puts("clear happened");
+    printf("empty %i\n", is_queue_empty(&qu));
+    enqueue(&qu, 6.4);
+    enqueue(&qu, 7.4);
+    enqueue(&qu, 8.4);
+    for (int i = 0; i < qu.index; i++)
+        printf("%f\n", qu.data[i]);
 }
