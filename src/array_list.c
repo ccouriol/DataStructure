996dce2ae04beaa6c345111762f52fb3c579159e
cgabette <sirdricks@outlook.com>
2021-10-15 10:24:15 +0200

    feat: Arraylist Datastructure done

---
+++ src/array_list.c
@@ -0,0 +1,123 @@
+/*!
+* \file array_list.c
+* \author GABETTE Cédric <sirdricks@outlook.com>
+* \version 0.1
+* \date 14/10/2021
+* \brief Contains all the functions for an array list
+* \remarks None
+* 
+* 
+* 
+*/
+
+/*! Importation of librairies*/
+#include "array_list.h"
+
+/*!
+* \fn void init_array_list(Array_list *l)
+* \author Name <email@email.com>
+* \version 0.1
+* \date  14/10/2021
+* \brief Initialize the list with values equals to 0
+* \remarks None
+* \param l 
+*/
+//TODO: Remplace the value 0 by a null value, to avoid confusion with a real value "0"
+void init_array_list(Array_list *l)
+{
+    for (int i = 0; i < ARRAY_LIST_MAX_SIZE; i++)
+    {
+        l->data[i] = 0;
+    }
+    l->index = 0;
+}
+
+/*!
+* \fn void insert_at(Array_list *l, int position, float value)
+* \author Name <email@email.com>
+* \version 0.1
+* \date  14/10/2021
+* \brief Put a value in a choosen position 
+* \remarks User can choose a position above the limit, resulting to put a value out of the list
+* \param l 
+* \param position 
+* \param value 
+*/
+//TODO: Position must be inferior to ARRAY_LIST_MAX_SIZE
+void insert_at(Array_list *l, int position, float value)
+
+    {
+        l->data[position] = value;
+    }
+
+/*!
+* \fn void add(Array_list *l, float value)
+* \author Name <email@email.com>
+* \version 0.1
+* \date  14/10/2021
+* \brief Put a value from the begining of the list, act like a stack
+* \remarks Can add a value out of the list if we reach the max limit
+* \param l 
+* \param value 
+*/
+//TODO: Prevent adding a value above ARRAY_LIST_MAX_SIZE delimitation
+void add(Array_list *l, float value)
+{
+    l->data[l->index] = value;
+    l->index++;
+}
+
+/*!
+* \fn float remove_at(Array_list *l, int position)
+* \author Name <email@email.com>
+* \version 0.1
+* \date  14/10/2021
+* \brief Delete the value from a choosen position
+* \remarks None
+* \param l 
+* \param position 
+* \return 
+*/
+//TODO: Position must be inferior to ARRAY_LIST_MAX_SIZE
+float remove_at(Array_list *l, int position)
+{
+    float toReturn = l->data[position];
+    l->data[position] = 0;
+    return toReturn;
+}
+
+/*!
+* \fn float get_at(Array_list *l,int position)
+* \author Name <email@email.com>
+* \version 0.1
+* \date  14/10/2021
+* \brief return the value of a choosen position
+* \remarks None
+* \param l 
+* \param position 
+* \return 
+*/
+//TODO: Position must be inferior to ARRAY_LIST_MAX_SIZE
+float get_at(Array_list *l, int position)
+{
+    return (l->data[position]);
+}
+
+/*!
+* \fn void clear_list(Array_list *l)
+* \author Name <email@email.com>
+* \version 0.1
+* \date  14/10/2021
+* \brief Clear the whole list by setting the values to 0
+* \remarks None
+* \param l 
+*/
+//TODO: Remplace the value 0 by a null value, to avoid confusion with a real value "0"
+void clear_list(Array_list *l)
+{
+    for (int i = 0; i < ARRAY_LIST_MAX_SIZE; i++)
+    {
+        l->data[i] = 0;
+    }
+    l->index = 0;
+}
--- src/main.c
+++ src/main.c
@@ -17,7 +17,9 @@
 #include <string.h>
 #include <assert.h>
 
-#include "../include/stack.h"
+#include "../include/array_list.h"
+
+void test_arraylist();
 
 /*!
 * \fn float test(int a)
@@ -50,8 +52,41 @@ float test(int a)
 * \param argv value of arguments given as inputs
 * \return 0 if all went good
 */
+
 int main(int argc, char **argv)
 {
 
+    test_arraylist();
+
     return (EXIT_SUCCESS);
+}
+
+void test_arraylist()
+{
+    float a0 = 1.123;
+    float b0 = 20.02;
+    float c0 = 12.12;
+
+    Array_list l0;
+    long l = &l0;
+
+    //Initialize the list : verify if the first index is 0
+    init_array_list(l);
+    assert(l0.index == 0);
+    //Put a value in a chosen position : verify if we find the value
+    insert_at(l, 3, 1.123);
+    assert(l0.data[3] == a0);
+    //Remplace the last value equal to 0 with a new value : verify if we find the value
+    add(l, 20.02);
+    assert(l0.data[l0.index - 1] == b0);
+    //Delete a value from a chosen position : verify if the value equals to 0
+    float returning = remove_at(l, 3);
+    assert(returning == a0);
+    //Add et return the value of a chosen position : verify if the value match
+    add(l, 12.12);
+    get_at(l, 1);
+    assert(l0.data[1] == c0);
+    //Set all the values to 0 et set index to 0 : verify is index equals to 0
+    clear_list(l);
+    assert(l0.index == 0);
 }
