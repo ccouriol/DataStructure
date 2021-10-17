a0cf46fc5da2d2a8888c926b12da1a7f3c877f31
ccouriol <clement.couriol@cpe.fr>
2021-10-17 15:07:53 +0200

    feat: heap data structure

--- include/heap.h
+++ include/heap.h
@@ -1,28 +1,29 @@
 #include <stdbool.h>
+#include <stdlib.h>
 
 #ifndef HEAP_H
 #define HEAP_H
 
-
 #define HEAP_MAX_SIZE 100
 
-typedef struct {
+typedef struct
+{
     float data[HEAP_MAX_SIZE];
     int index;
 } Heap;
-
 
 #endif /* HEAP_H */
 
 void init_heap(Heap *h);
 bool is_heap_empty(Heap *h);
-float pop(Heap *h); //pop the root value
-void push(Heap *h, float value);
+float pop_heap(Heap *h); //pop the root value
+void push_heap(Heap *h, float value);
+void pupupu(Heap *h, float value);
 float replace(Heap *h, float value); // pop root and push a new key.
-float peek(Heap *h); // return root value but dont remove it
+float peek_heap(Heap *h);            // return root value but dont remove it
 
 //En option
-void heapify(Heap *s, float array[], size_t array_size); //create Heap from array
-void merge(Heap *dest, Heap *src); //merge src into dest
+void heapify(Heap *h, float array[], size_t array_size); //create Heap from array
+void merge(Heap *dest, Heap *src);                       //merge src into dest
 
-
+void heapsort(Heap *h);
